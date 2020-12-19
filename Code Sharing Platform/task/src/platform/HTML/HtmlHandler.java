19bd7959b2fb436f5c1c47d6ad46d6b54df02970
Maxim <70700564+MaksimAbroskin@users.noreply.github.com>
2020-12-19 20:27:44 +0300

    stage 4 completed. Deleted useless deprecated classes and methods

--- Code Sharing Platform/task/src/platform/HTML/HtmlHandler.java
+++ Code Sharing Platform/task/src/platform/HTML/HtmlHandler.java
@@ -1,6 +1,5 @@
 package platform.HTML;
 
-import platform.DatabaseSharedCode;
 import platform.JSON.JsonObject;
 import platform.fileHandler.FileHandler;
 
@@ -10,15 +9,6 @@ public class HtmlHandler {
     final static String CODE = "#SHARED_CODE#";
     final static String TIME_OF_CHANGE = "#TIME_OF_CHANGE#";
     final static String LATEST = "#LATEST#";
-
-    public static String responseTo_getCodeNumber(Path pathToTemplate, DatabaseSharedCode database, int number) {
-        JsonObject note = database.getNoteByNumber(number);
-        String template = FileHandler.readFileToString(pathToTemplate);
-        if (note != null) {
-            return template.replace(CODE, note.getCode()).replace(TIME_OF_CHANGE, note.getDate());
-        }
-        return template;
-    }
 
     public static String wrapJsonObjectToHtml(Path pathToTemplate, JsonObject jsonObject) {
         String template = FileHandler.readFileToString(pathToTemplate);
@@ -40,19 +30,6 @@ public class HtmlHandler {
 
     public static String responseTo_getCodeNew(Path pathToTemplate) {
         return FileHandler.readFileToString(pathToTemplate);
-    }
-
-    public static String responseTo_getCodeLatest(Path pathToTemplate, Path pathToInnerTemplate, DatabaseSharedCode database) {
-        String template = FileHandler.readFileToString(pathToTemplate);
-        String innerTemplate = FileHandler.readFileToString(pathToInnerTemplate);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = database.getSize() - 1; i > database.getSize() - 11; i--) {
-            if (i < 0) break;
-            stringBuilder.append(innerTemplate.replace(CODE, database.getNoteByNumber(i).getCode()).replace(TIME_OF_CHANGE, database.getNoteByNumber(i).getDate()));
-        }
-
-        return template.replace(LATEST, stringBuilder);
     }
 
 }
